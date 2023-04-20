8be667b730affd075abb76a6a36480fd1d2e9198
c4rlosca <soycarloscaraballo@gmail.com>
2023-04-20 23:01:39 +0200

    header con pruebas

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,26 @@
-console.log('Hello World!');
+let sideMenu = document.querySelector(".side");
+let closeTab = sideMenu.firstElementChild;
+let burger = document.querySelector(".burger");
+burger.addEventListener("click", () => {
+  sideMenu.classList.remove("hidden");
+});
+closeTab.addEventListener("click", () => {
+  sideMenu.classList.add("hidden");
+});
+let header = document.querySelector("header");
+let ScrollPosition = 0;
+
+window.addEventListener("scroll", () => {
+  const currentScrollPosition = window.pageYOffset;
+
+  if (currentScrollPosition > ScrollPosition) {
+    header.classList.remove("up");
+  } else {
+    header.classList.add("up", "readable");
+    setTimeout(() => {
+        header.classList.remove("readable")
+    }, 200);
+  }
+
+  ScrollPosition = currentScrollPosition;
+});
